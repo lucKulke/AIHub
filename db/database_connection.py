f2f922895e0f55be9cbc28e3b71999adfbcb7d3f
lucKulke <lucaskulke98@gmail.com>
2023-11-27 14:35:13 +0100

    changed env vars and adapted the code

--- db/database_connection.py
+++ db/database_connection.py
@@ -3,14 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
+db_type = os.getenv("DB_TYPE")
+db_username = os.getenv("DB_USERNAME")
 db_password = os.getenv("DB_PASSWORD")
 db_host = os.getenv("DB_HOST")
 db_name = os.getenv("DB_NAME")
 db_port = os.getenv("DB_PORT")
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://postgres:{db_password}@{db_host}:{db_port}/{db_name}"
+    f"{db_type.lower()}://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
