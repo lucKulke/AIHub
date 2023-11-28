beba6197f7c3f4d2d9cc5e37f7a57aaeecbef151
lucKulke <lucaskulke98@gmail.com>
2023-11-28 15:13:32 +0100

    admin full access to all routes

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, routing
 from db import database_connection
 from db import models
 from routers import language_processing, text_to_voice, voice_to_text, image_generation
-from security import security_routes
+from security import security_routes, handler
+import re
 
 app = FastAPI()
 
@@ -10,6 +11,20 @@ app = FastAPI()
 engine = database_connection.engine
 
 models.Base.metadata.create_all(bind=engine)
+
+
+@app.on_event("startup")
+async def startup_event():
+    all_available_paths = []
+    for route in app.routes:
+        if isinstance(route, routing.APIRoute):
+            match = re.search(r"/([^/]+)/?$", route.path)
+            if match:
+                last_word = match.group(1)
+                all_available_paths.append(last_word)
+
+    handler.fake_users_db["lucaskulke"]["scopes"] = all_available_paths
+    print(all_available_paths, flush=True)
 
 
 app.include_router(security_routes.router)
