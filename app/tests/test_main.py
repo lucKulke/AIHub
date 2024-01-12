04a94fdf54aa85e41a50e1014af0f74bf49b906a
lucKulke <lucaskulke98@gmail.com>
2024-01-12 21:19:24 +0100

    restructured project + added testing

---
+++ app/tests/test_main.py
@@ -0,0 +1,23 @@
+import sys
+from pathlib import Path
+
+# Add the parent directory of 'app' to the Python path
+sys.path.append(str(Path(__file__).parent.parent))
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+client = TestClient(app)
+
+
+def test_azure_available_voices():
+    response = client.get("/text_to_voice/azure/available_voices")
+    assert response.status_code == 200
+    # assert response.json() == {"msg": "Hello World"}
+
+
+def test_authentication():
+    response = client.post("/text_to_voice/azure")
+    assert response.status_code == 401
